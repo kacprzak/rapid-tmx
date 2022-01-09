718e96836ee38f7f6de5620b706d8a2f17589a32
Marcin Kacprzak <martin.kacprzak@gmail.com>
2022-01-09 20:05:07 +0100

    Get version from build recip.

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,19 @@
 from conans import ConanFile, Meson, tools
+import re, os
 
 class RapidTmxConan(ConanFile):
     name = "rapid-tmx"
-    version = "0.1"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
     generators = "pkg_config"
     exports_sources = "meson.build", "include/*", "src/*", "external/*", "tests/*"
     requires = "zlib/1.2.11", "rapidxml/1.13"
+
+    def set_version(self):
+        content = tools.load(os.path.join(self.recipe_folder, "meson.build"))
+        version = re.search(r"version\s*:\s*'([\d.]+)'", content).group(1)
+        self.version = version.strip()
 
     def config_options(self):
         if self.settings.os == "Windows":
